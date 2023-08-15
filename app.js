7a605323a7c868c16c073dd655a3bff12d5932da
amit-chaman <amitchaman369@gmail.com>
2023-08-15 15:08:52 +0530

    added chalk

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const getNotes = require ('./notes');
-
+const validator = require('validator');
+const chalk = require('chalk');
 const msg = getNotes();
 
 console.log(msg);
-
-
-
+console.log(validator.isEmail('abc@example.com'));
+console.log(chalk.blue('hello world'));
+console.log(chalk.red('hello world'));
+console.log(chalk.green('hello world'));
 // const add = require('./utils');
 
 // const sum = add(4,-2);
